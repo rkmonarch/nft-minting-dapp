243bccbc31611337e0b03d23b75dbe40fd3c4a4b
Rahul <rahulkulkarni5234@gmail.com>
2022-12-09 10:23:11 +0530

    Added font-end

--- src/App.js
+++ src/App.js
@@ -1,18 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useAccount, useContract, useSigner } from "wagmi";
+import { abi as CONTRACT_ABI } from "./ABI";
+const CONTRACT_ADDRESS = "0x3D23D647Bf5BCCdA1801dA57C068E7FEcEa4336e";
 
 function App() {
+	const { address } = useAccount();
+	const { data: signer } = useSigner();
+	const [mintRate, setMintRate] = useState("0");
+	const [error, setError] = useState(false);
+	const [tokenId, setTokenId] = useState(null);
+	const [loading, setLoading] = useState(false);
+
+	const contract = useContract({
+		addressOrName: CONTRACT_ADDRESS,
+		contractInterface: CONTRACT_ABI,
+		signerOrProvider: signer,
+	});
+
+	console.log(contract);
+	const getMint = async () => {
+		const price = await contract.mintRate();
+		setMintRate(price.toString());
+	};
+
+	useEffect(() => {
+		if (contract?.signer) {
+			getMint();
+		}
+	}, [contract]);
+
+	const mintNft = async () => {
+		setLoading(true);
+		try {
+			const mint = await contract.safeMint({ value: mintRate });
+			console.log(mint);
+
+			contract.on("Transfer", (to, from, token) => {
+				console.log(to, from, token);
+				setTokenId(token.toString());
+				setLoading(false);
+			});
+		} catch (err) {
+			console.log(err);
+			setError(err);
+			setLoading(false);
+		}
+	};
+
+	console.log("MINT ", mintRate);
+	console.log("TOKEN ", tokenId);
+	console.log("ERROR ", error);
+	console.log("LOADING ", loading);
+
 	return (
-		<div className="flex items-center justify-center min-h-screen">
-			<div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-4xl font-extrabold">Mint an NFT</h1>
-        <button className="bg-black text-white py-2 px-4 rounded-xl transform hover:scale-105">
-          Mint
-        </button>
-      </div>
+		<div>
+			<div className="flex items-center justify-end p-2">
+				<ConnectButton />
+			</div>
+			<div className="flex flex-col items-center justify-center min-h-[90vh] gap-4">
+				<h1 className="text-4xl font-extrabold">Mint an NFT</h1>
+				{address ? (
+					loading ? (
+						<h3 className="text-xl">Minting your NFT...</h3>
+					) : (
+						<button
+							onClick={mintNft}
+							className="bg-black text-white py-2 px-4 rounded-xl transform hover:scale-105"
+						>
+							Mint
+						</button>
+					)
+				) : (
+					<ConnectButton />
+				)}
+				{error && (
+					<div>
+						<p>Something went wrong!</p>
+					</div>
+				)}
+				{tokenId && (
+					<div className="flex flex-col">
+						<p className="font-bold">Token with id {tokenId} minted</p>
+						<p>
+							Check it over here:{" "}
+							<a
+								href={`https://testnets.opensea.io/assets/goerli/${CONTRACT_ADDRESS}/${tokenId}`}
+                target="_blank"
+                rel="noreferrer"
+							>
+								Opensea
+							</a>
+						</p>
+					</div>
+				)}
+			</div>
 		</div>
 	);
 }
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,46 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@rainbow-me/rainbowkit/styles.css';
 
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public'; 
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.goerli, chain.polygonMumbai],
+  [
+    alchemyProvider({ apiKey: "wO9ZmXZhJlPksbLT3Id4Uyd6OS3x54aC" }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My NFT Minting DAPP',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
   </React.StrictMode>
 );
 
